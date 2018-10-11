28c223e99b419650c46ddf3e6e8859ec03eb1dc3
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2018-10-11 23:16:29 +0900

    apache

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 test -z $(which ansible-playbook) && sudo yum install -y epel-release && sudo yum install -y ansible
 sudo yum update -y ansible
 ansible-playbook main.yml --connection=local
+ansible-playbook 01-apache.yml --connection=local
