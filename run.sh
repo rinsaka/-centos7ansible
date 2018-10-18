bbb607df60b4dd662900b151c358296448577dd9
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2018-10-18 16:32:25 +0900

    mysql

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ ansible-playbook main.yml --connection=local
 ansible-playbook 01-apache.yml --connection=local
 ansible-playbook 02-ldapserver.yml --connection=local
 ansible-playbook 03-ldif.yml --connection=local
+ansible-playbook 04-mysql.yml --connection=local
